3a0a82a2ec88ad912f19f954ffce955b22c255b9
Eric Turner <eturner@gamingarts.com>
2017-05-03 09:27:40 -0700

    add markers to map

--- gpstrack/static/js/home.js
+++ gpstrack/static/js/home.js
@@ -1,6 +1,7 @@
 var map;
 tracks = {};
-
+var iconBase = static_url + 'images/markers/';
+icons = {};
 
 function init() {
     getTracks().done(function (res, status) {
@@ -11,8 +12,8 @@ function init() {
             firstTrack[firstTrack.length - 1]['location']['lon'])
     }).done(function () {
         for (var i = 1; i < tracks.length; i++) {
-        plotTrack(tracks[i])
-    }
+            plotTrack(tracks[i])
+        }
     })
 }
 
@@ -24,25 +25,73 @@ function initMap(lat, lon) {
         mapTypeId: 'terrain'
 
     });
+
+    icons = {
+        black_marker: {
+            url: iconBase + 'marker_black.png',
+            scaledSize: new google.maps.Size(6, 6),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(3, 3)
+        },
+        red_marker: {
+            url: iconBase + 'marker_red.png',
+            scaledSize: new google.maps.Size(6, 6),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(3, 3)
+        },
+        green_marker: {
+            url: iconBase + 'marker_green.png',
+            scaledSize: new google.maps.Size(6, 6),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(3, 3)
+        },
+        yellow_marker: {
+            url: iconBase + 'marker_yellow.png',
+            scaledSize: new google.maps.Size(6, 6),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(3, 3)
+        },
+        blue_marker: {
+            url: iconBase + 'marker_blue.png',
+            scaledSize: new google.maps.Size(6, 6),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(3, 3)
+        }
+    };
+
+
+
 }
 
 function plotTrack(track) {
     var points = [];
-    for (var i = 0; i < track['points'].length; i++) {
-        points.unshift({
-            lat: track['points'][i]['location']['lat'],
-            lng: track['points'][i]['location']['lon']
-        });
-        var route = new google.maps.Polyline({
-            path: points,
-            geodesic: true,
-            strokeColor: '#FF0000',
-            strokeOpacity: 1.0,
-            strokeWeight: 2
-        });
 
-        route.setMap(map);
-    }
+    track['points'].forEach(function (point) {
+            points.unshift(
+                new google.maps.LatLng(point['location']['lat'], point['location']['lon'])
+                // lat: point['location']['lat'],
+                // lng: point['location']['lon']
+            )
+        }
+    );
+    var route = new google.maps.Polyline({
+        path: points,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+
+    points.forEach(function (point) {
+        // var latLong = new google.maps.LatLng(,)
+        var marker = new google.maps.Marker({
+            position: point,
+            icon: icons['red_marker'],
+            map: map
+        });
+        marker.setMap(map);
+    });
+    route.setMap(map);
 }
 
 
