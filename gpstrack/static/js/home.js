03e89e5d7432afaeaae9b24ae77ae3a7bc672061
Eric Turner <eturner@gamingarts.com>
2017-05-05 17:57:32 -0700

    info box displaying some information

--- gpstrack/static/js/home.js
+++ gpstrack/static/js/home.js
@@ -135,78 +135,58 @@ function getPointsForTrack(trackPK) {
 }
 
 function createContentString(point, track) {
-    return `<div class="leaflet-popup-content-wrapper"><div class="leaflet-popup-content" style="width: 193px;"><div id="divPopup" class="container-fluid" style="margin:-10px; padding-right:2px; padding-left:2px; min-width:140px;">' +
-	'<div style="padding-bottom:5px; padding-left;10px">'
-
+    console.log(point);
+    console.log(track);
+    return `<div class="leaflet-popup-content-wrapper"><div class="leaflet-popup-content" style="width: 193px;"><div id="divPopup" class="container-fluid" style="margin:-10px; padding-right:2px; padding-left:2px; min-width:140px;">
+	<div style="padding-bottom:5px; padding-left:10px;">
 		<div>
-			<div data-bind="text: displayName" style="font-size:14px;overflow:hidden;text-overflow:ellipsis; white-space:nowrap">` + track.user.profile.display_name + `</div>
+			<div style="font-size:14px;overflow:hidden;text-overflow:ellipsis; white-space:nowrap">` + track.user.profile.display_name + `</div>
 		</div>
 		<div style="color:#999999;">
-			<span style="float:left;font-size:11px;" data-bind="text:  messageDate">Apr 30, 2017</span>
-			<span style="float:right;font-size:11px;" data-bind="text: messageTime">3:58:48 PM</span>
+			<span style="float:left;font-size:11px;" data-bind="text:  messageDate">` + new Date(point.time.local_Time).toDateString() + `</span>
+			<span style="float:right;font-size:11px;" data-bind="text: messageTime">` + new Date(point.time.local_Time).toTimeString() + `</span>
 		</div>
-
-
 		<div style="clear: both;"></div>
 		<br>
-
-		<div data-bind="visible: function () { return message() != null &amp;&amp; message() != '' }">
-			<span style="font-size: 12px; overflow: hidden; text-overflow: ellipsis;word-wrap:break-word;" data-bind="text: message"></span>
+		<div>
+			<span style="font-size: 12px; overflow: hidden; text-overflow: ellipsis;word-wrap:break-word;">` + 'a message, if a message' + `</span>
 			<br>
 		</div>
-		<div data-bind="visible: Debug()==true" style="display: none;">
-			Id: <span data-bind="text: sourcePoint().Id">0</span><br>
-			Trail: <span data-bind="text: sourcePoint().T">3391051</span>
+		<div style="display: none;">
+			Track Id: <span>` + track.id + `</span><br>
+			Track: <span>` + track.name + `</span>
 		</div>
 	</div>
-	<div class="row-fluid" data-bind="visible: displayMore" style="padding-bottom: 10px;">
+	<div class="row-fluid" style="padding-bottom: 10px;">
 		<table>
 			<tbody>
 				<tr style="white-space:nowrap">
 					<td>
-						<span style="color:gray;font-size:11px;">Speed: <span style="white-space:nowrap;font-size:11px;color:black" data-bind="text: speed">0.62 mph</span>
+						<span style="color:gray;font-size:11px;">Speed: <span style="white-space:nowrap;font-size:11px;color:black">` + point.velocity + ` mph</span>
 					</span></td>
 					<td style="padding-left:12px;">
-						<span style="color:gray;font-size:11px;">Course: <span style="white-space:nowrap;font-size:11px;color:black" data-bind="html: heading">N</span>
+						<span style="color:gray;font-size:11px;">Course: <span style="white-space:nowrap;font-size:11px;color:black">` + point.course + `</span>
 					</span></td>
 				</tr>
 				<tr style="white-space:nowrap">
 					<td>
-						<span style="color:gray;font-size:11px;">Elevation: <span style="white-space:nowrap;font-size:11px;color:black" data-bind="text: altitude">1,072.83 ft.</span>
+						<span style="color:gray;font-size:11px;">Elevation: <span style="white-space:nowrap;font-size:11px;color:black">` + point.location.elevation + ` ft.</span>
 					</span></td>
 					<td style="padding-left:12px;">
-						<span style="color: gray; font-size: 11px; display: none;" data-bind="visible: batt()!=''">Batt: <span style="white-space:nowrap;font-size:11px;color:black" data-bind="text: batt"></span>
-					</span></td>
-				</tr>
-				<tr style="white-space:nowrap" data-bind="visible: oneCoord()==false">
-					<td>
-						<span style="color:gray;font-size:11px;">Lat: <span style="white-space:nowrap;font-size:11px;color:black" data-bind="text: lat">36.038754</span>
-					</span></td>
-					<td style="padding-left:12px;">
-						<span style="color:gray;font-size:11px;">Lon: <span style="white-space:nowrap;font-size:11px;color:black" data-bind="text: lon">-114.782291</span>
+						<span style="color: gray; font-size: 11px; display: none;" data-bind="visible: batt()!=''">Batt: <span style="white-space:nowrap;font-size:11px;color:black">batt??</span>
 					</span></td>
 				</tr>
 				<tr style="white-space:nowrap">
-					<td colspan="2" data-bind="visible: oneCoord()==true, text: coords()[0]" style="color: gray; font-size: 11px; display: none;">36.038754</td>
+					<td>
+						<span style="color:gray;font-size:11px;">Lat: <span style="white-space:nowrap;font-size:11px;color:black">` + point.location.lat + `</span>
+					</span></td>
+					<td style="padding-left:12px;">
+						<span style="color:gray;font-size:11px;">Lon: <span style="white-space:nowrap;font-size:11px;color:black">` + point.location.lon + `</span>
+					</span></td>
 				</tr>
 			</tbody>
 		</table>
 	</div>
-	<div class="row-fluid" style="text-align:center;">
-		<span style="position:relative">
-			<input type="image" title="Activate tracking on this user's inReach." data-bind="click: showTrackingDlg, attr: { src: trackingButtonImage }, enable: userListBtnEnabled, visible: EnableTracking(), css: { paddedBubble: displayMore() == false }" src="/content/images/Popup/Track-Big.png" class="" style="display: none;">
-			<span class="popupButtonText" data-bind="visible: displayMore() &amp;&amp; EnableTracking()" style="display: none;">Track</span>
-		</span>
-		<span style="position:relative">
-			<input type="image" title="Send a message to this user." data-bind="click: showMessageDlg, attr: { src: messageButtonImage }, enable: userListBtnEnabled, visible:EnableMessage(), css: { paddedBubble: displayMore() == false }" src="/content/images/Popup/Message-Big.png" class="">
-			<span class="popupButtonText" data-bind="visible: displayMore() &amp;&amp; EnableMessage()">Message</span>
-		</span>
-		<span style="position:relative">
-			<input type="image" title="Request the location of this user." data-bind="click: showLocateDlg, attr: { src: locateButtonImage }, enable: userListBtnEnabled, visible:EnableLocate(), css: { paddedBubble: displayMore() == false }" src="/content/images/Popup/Locate-Big.png" class="" style="display: none;">
-			<span class="popupButtonText" data-bind="visible: displayMore() &amp;&amp; EnableLocate()" style="display: none;">Locate</span>
-		</span>
-	</div>
-	<div style="text-align: center; cursor: pointer; color: inherit;" data-bind="html: (displayMore() ? '▼ LESS ▼' : '▲ MORE ▲'), click: function () { displayMore() ? displayMore(false) : displayMore(true) }, style: { color: (displayMore() ? 'inherit' : '#0080FF') }">▼ LESS ▼</div>
 </div></div></div>`
 
 }
